5c78a3027215769639a0cc07783e31407008efc4
arthur mattjie <162368460+artego974@users.noreply.github.com>
2024-07-06 22:41:34 -0300

    Add files via upload

--- escola.ts
+++ escola.ts
@@ -1,105 +1,92 @@
-let r = require("readline-sync")
-class prof {
+let rl = require("readline-sync")
+class professor{
     nome: string
-    idade:number
-    ano_xp:number
-    
-    constructor(nome:string,idade:number,ano_xp:number) {
+    idade: number
+    salario: number
+    anos_xp:number
+    matéria:number
+    constructor(nome:string,idade:number,salario:number,anos_xp:number,matéria:number){
         this.nome = nome
         this.idade = idade
-        this.ano_xp = ano_xp
-    }
-    getpf():void{
-        console.log("sou o ", this.nome," tenho ",this.idade," e tenho ",this.ano_xp," de experiencia")
-    }
-    setpf():void{
-        let nomeN = r.question("novo nome")
-        let idadeN = parseInt(r.question("nova idade"))
-        let ano_xpN = parseInt(r.question("novo ano de experiencia"))
+        this.salario = salario
+        this.anos_xp = anos_xp
+        this.matéria = matéria
+        }
+        darAula():void{
+            console.log("Professor "+this.nome+" está dando aula de "+this.matéria)
+        }
+        curriculo():void{
+            console.log("Nome: "+this.nome+"\nIdade: "+this.idade+"\nSalário: "+this.salario+"\nAnos de experiência: "+this.anos_xp+"\nMatéria: "+this.matéria)
+        }
+        novoProf(){
+        let nomeN = rl.question("novo nome")
+        let idadeN = parseInt(rl.question("nova idade"))
+        let anos_xpN = parseInt(rl.question("novo ano de experiencia"))
+        let salario = parseInt(rl.question("qual o seu salario"))
+        let matéria = parseInt(rl.question("qual a sua matéria"))
         this.nome = nomeN
         this.idade = idadeN
-        this.ano_xp = ano_xpN
-    }
-
-}
-class school {
-    profer:prof
-    nome:string
-    endereco:string
-    num_end:number
-    constructor(profer:prof,nome:string,endereco:string,num_end:number) {
-
-        this.profer = profer
-        this.nome = nome
-        this.endereco = endereco
-        this.num_end = num_end
-        
-    }
-    getesc():void{
-        console.log("sou a escola ",this.nome," localizado em ",this.endereco,"numero ",this.num_end," e tenho o prof ",this.profer.nome)
+        this.anos_xp = anos_xpN
+        this.salario = salario
+        this.matéria = matéria
         }
-        setesc():void{
-            this.profer.setpf()
-            let nomeN = r.question("novo nome da escola")
-            let enderecoN = r.question("novo endereco da escola")
-            let num_endN = (r.questionInt("novo numero do endereco da escola"))
-            this.nome = nomeN
-            this.endereco = enderecoN
-            this.num_end = num_endN
-            this.profer.setpf()
-            }
+
 }
+class alunos{
+    nome:string
+    idade:number
+    mensalidade:number
+    constructor(nome:string,idade:number,mensalidade:number){
+        this.nome = nome
+        this.idade = idade
+        this.mensalidade = mensalidade
+        }
+        getalunos(){
+            console.log("Nome: "+this.nome+"\nIdade: "+this.idade+"\nMensalidade: "+this.mensalidade)
+                }
+            setalunos(){
+                    let nome = rl.question("qual o seu nome")
+                    let idade = parseInt(rl.question("qual a sua idade"))
+                    let mensalidade = parseInt(rl.question("qual a sua mensalidade"))
+                    this.nome = nome
+                    this.idade = idade
+                    this.mensalidade = mensalidade
+                }
+        }
 
-let profc = new prof("", 0,0)
-let mal = new school (profc,"","",0)
 
-console.log(mal)
-
-
-profc.getpf()
-profc.setpf()
-profc.getpf()
-
-
-let n = true
-
-let NovProf: Array<prof> = []
-let NovEsc: Array<school> = []
-
-while (n == false) {
-    let opc = r.questionInt("qual a opção? \n[1]criar professor\n[2]criar nova escola\n[3]mudar prof\n[4]mudar escola\n[5]ver prof\n[6]ver escola\n[7]sair\n")
-
-    switch (opc) {
-        case 1:
-            profc.setpf()
-            NovProf.push(profc)
-
-            break;
-            case 2:
-                mal.setesc()
-                NovEsc.push(mal)
-                break;
-                case 3:
-                    profc.setpf()
-                    
-                    break;
-                    case 4:
-                        mal.setesc()
-                        break;
-                        case 5:
-                            console.log(NovProf)
-                            
-                            break;
-                            case 6:
-                                console.log(NovEsc)
-                
-                                break;
-                        case 7:
-            n = false
-            break;
-    
-        default:
-            console.log("invalido essa opção")
-            break;
+class sala_de_aula {
+    professor: professor[]
+    qntd_alunos: number
+    constructor(professor: professor[],qntd_alunos:number) {
+        this.professor = professor
+        this.qntd_alunos = qntd_alunos
+        }
+        troca_de_periodo():void{
+            console.log("Professor "+this.professor[0].nome+" está dando aula de "+this.professor[0].matéria+" para "+this.qntd_alunos+" alunos")
+        }
+  
+}
+class diretor {
+    nome: string
+    idade: number
+    salario: number
+    anos_xp: number
+    professor:professor
+    alunos:alunos
+    constructor(nome:string,idade:number,salario:number,anos_xp:number){
+        this.nome = nome
+        this.idade = idade
+        this.salario = salario
+        this.anos_xp = anos_xp
     }
+    admEscola(){
+        let lucro 
+        let gastos 
+        let lucro_ano = lucro - gastos
+
+
+    }
+
+
 }
